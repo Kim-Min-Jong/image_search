93f71749a0a7448e8d2da6ffe93b0c235ae81d37
minjong kim <alswhddkdl@gmail.com>
2023-09-12 22:59:04 +0900

    [FEAT]: bottom navigation view 리스너 구현

---
+++ app/src/main/java/com/sparta/imagesearch/view/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.sparta.imagesearch.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.sparta.imagesearch.R
+import com.sparta.imagesearch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        bottomNavView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    showFragment(SearchFragment.newInstance(), SearchFragment.TAG)
+                    true
+                }
+                R.id.storage -> {
+                    showFragment(SaveFragment.newInstance(), SaveFragment.TAG)
+                    true
+                }
+                else -> false
+            }
+
+        }
+        bottomNavView.selectedItemId = R.id.home
+    }
+
+    private fun showFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, fragment, tag)
+            .commit()
+    }
+
+}
