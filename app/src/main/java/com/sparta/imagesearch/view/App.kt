4a8bf19bd687e6d480ef1f9c2245c954f1f46063
minjong kim <alswhddkdl@gmail.com>
2023-10-15 13:46:17 +0900

    test

--- app/src/main/java/com/sparta/imagesearch/view/App.kt
+++ app/src/main/java/com/sparta/imagesearch/view/App.kt
@@ -3,11 +3,13 @@ package com.sparta.imagesearch.view
 import android.app.Application
 import com.sparta.imagesearch.preference.PreferenceUtils
 
-class App: Application() {
+class App : Application() {
     override fun onCreate() {
         prefs = PreferenceUtils(applicationContext)
         super.onCreate()
+        //
     }
+
     companion object {
         lateinit var prefs: PreferenceUtils
     }
